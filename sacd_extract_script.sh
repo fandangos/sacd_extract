463b99592a14b03d0cfd9f8b476ea6ca3598b97f
fandangos <veggav@gmail.com>
2022-08-18 12:48:49 -0300

    improved security and script

--- sacd_extract_script.sh
+++ sacd_extract_script.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# This script is used to find any iso files (case insensitive) and use sacd_extract tool to extract all the dsf files from it.
+# This script is used to find any iso files (case insensitive) and use sacd_extract tool
+# to extract all the dsf files from it.
 # sacd is set to output as DSF and remove the iso file after the extraction is done.
 # since sacd_extract outputs to a directory I needed it to move the files to it's parents directory
 # later remove the emtpty directory and fix permissions
 #
-# Very user dependent and I made it for my personal use and might be useful for someone else.
-# requires sacd_extract to be available system wide (move it to /usr/bin)
+# It exports the current directory as a path so sacd_extract is available whenever it moves folders
+# searching for the iso files.
+# It also sets sacd_extract binary as executable.
+#
+# This script requires root.
+
+IFS=$'n\t'
+export PATH=$PATH:$(pwd)
+chmod +x "sacd_extract"
 
 find . -iname "*.iso" -type f -exec bash -c '
+IFS=$'n\t'
    cd "${1%/*}"
      echo "$PWD"
         file="${1##*/}"
@@ -17,16 +26,15 @@ find . -iname "*.iso" -type f -exec bash -c '
 
 find . -iname "*.dsf" -type f | \
       while read I
-        do
+       do
+          echo extracted file: "$I"
+          chmod 666 "$I"
           mv "$I" "${I%/*/*}"
        done
 
    find . -type d -empty -delete
 
-   chmod -R 777 *
-
 ' bash {} \;
-
 
 
 
